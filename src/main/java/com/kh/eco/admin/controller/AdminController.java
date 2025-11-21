ba1831c55fdce4bc62a033f1fa5f285f0bda71c0
Ahnjunyoung927 <younge64@naver.com>
2025-11-21 17:01:05 +0900

    findBoardAndCommentByAdmin

--- src/main/java/com/kh/eco/admin/controller/AdminController.java
+++ src/main/java/com/kh/eco/admin/controller/AdminController.java
@@ -1,21 +1,49 @@
 package com.kh.eco.admin.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kh.eco.admin.model.dto.AdminBoardDTO;
+import com.kh.eco.admin.model.dto.AdminCommentDTO;
+import com.kh.eco.admin.model.service.AdminService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
+@Validated
 @RequestMapping("admin")
 @RequiredArgsConstructor
 public class AdminController {
 	
+	private final AdminService adminService;
 	
+
+	@GetMapping("boards")
+	public ResponseEntity<List<AdminBoardDTO>> findBoardAll(@RequestParam(name="page", defaultValue="0") int pageNo ){
+		List<AdminBoardDTO> boards = adminService.findBoardAll(pageNo);
+		return ResponseEntity.ok(boards);
+	}
 	
+	@GetMapping("comments")
+	public ResponseEntity<List<AdminCommentDTO>> findCommentAll(@RequestParam(name="page", defaultValue="0") int pageNo){
+		List<AdminCommentDTO> comments = adminService.findCommentAll(pageNo);
+		return ResponseEntity.ok(comments);
+	}
 	
-	
+	@DeleteMapping("boards")
+	public ResponseEntity<?> deleteBoard(@RequestParam(name="boardNo") Long boardNo){
+		adminService.deleteBoard(boardNo);
+		return ResponseEntity.ok("게시글이 비공개처리 되었습니다.");
+	}
 	
 	
 	
--- src/main/java/com/kh/eco/admin/model/service/AdminService.java
+++ src/main/java/com/kh/eco/admin/model/service/AdminService.java
@@ -1,5 +1,27 @@
 package com.kh.eco.admin.model.service;
 
+import java.util.List;
+
+import com.kh.eco.admin.model.dto.AdminBoardDTO;
+import com.kh.eco.admin.model.dto.AdminCommentDTO;
+import com.kh.eco.common.PageInfo;
+
 public interface AdminService {
+	
+	
+	List<AdminBoardDTO> findBoardAll(int pageNo);
+	
+	AdminBoardDTO findByBoardNo(Long boardNo);
+	
+	List<AdminCommentDTO> findCommentAll(int pageNo);
+	
+	AdminCommentDTO findByCommentNo(Long boardNo);
+	
+	void deleteBoard(Long boardNo);
+	
+	// int deleteByBoardNo(Long boardNo);
+	// 삭제는 게시판 담당자가 만든 기능에 중복될것 같으니 일단 구현 대기
+	// int deleteByCommentNo(Long commentNo);
+	
 	
 }
