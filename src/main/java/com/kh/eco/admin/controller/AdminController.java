2e2aafeba4041897fbc6358ee49a37ca7fda3812
Ahnjunyoung927 <younge64@naver.com>
2025-11-28 16:53:08 +0900

    removeUnuseImport

--- src/main/java/com/kh/eco/admin/controller/AdminController.java
+++ src/main/java/com/kh/eco/admin/controller/AdminController.java
@@ -1,7 +1,5 @@
 package com.kh.eco.admin.controller;
 
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kh.eco.admin.model.dto.AdminCommentDTO;
 import com.kh.eco.admin.model.dto.CommentPageResponse;
 import com.kh.eco.admin.model.dto.PageResponse;
 import com.kh.eco.admin.model.service.AdminService;
