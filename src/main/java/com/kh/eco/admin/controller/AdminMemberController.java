9b450b0190376d10a8dcb15f480f7afcb13fe270
Ahnjunyoung927 <younge64@naver.com>
2025-12-02 14:45:20 +0900

    commitBeforeMerge

--- src/main/java/com/kh/eco/admin/controller/AdminMemberController.java
+++ src/main/java/com/kh/eco/admin/controller/AdminMemberController.java
@@ -1,5 +1,17 @@
 package com.kh.eco.admin.controller;
 
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@Validated
+@RequestMapping("admin/comments")
+@RequiredArgsConstructor
 public class AdminMemberController {
 
 }
